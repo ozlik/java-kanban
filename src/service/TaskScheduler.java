625d08914f0e59959ddc6fbf0491867e7b8466bd
ozlik <brotherhood2@yandex.ru>
2024-10-29 09:58:55 +0500

    Добавить резервное копирование задач в файл. Спринт 8. UPD 27.10 1. Добавить учет времени в задачи (время начала, завершения, длительность). 2. Создать метод учета времени у Эпиков в классе InMemoryTaskManager. 3. Добавить валидацию времени задач на пересечение по логике: Предполагается, что пользователь будет выполнять не более одной задачи за раз. 4. Оптимизированы методы под Stream API. 5. Реализовать логику проверки пересечений через предварительно сетку расписания.

--- src/service/TaskScheduler.java
+++ src/service/TaskScheduler.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TaskScheduler {
-    final static int INTERVAL_MINUTES = 15;
-    final static LocalDateTime START_DATE = LocalDateTime.of(2024, 1, 1, 0, 0);
-    final static LocalDateTime END_DATE = START_DATE.plusYears(1);
+    static final int INTERVAL_MINUTES = 15;
+    static final LocalDateTime START_DATE = LocalDateTime.of(2024, 1, 1, 0, 0);
+    static final LocalDateTime END_DATE = START_DATE.plusYears(1);
 
     public Map<LocalDateTime, Boolean> scheduleTimeSlots() {
         Map<LocalDateTime, Boolean> timeSlots = new HashMap<>();
@@ -19,13 +19,11 @@ public class TaskScheduler {
         return timeSlots;
     }
 
-    public LocalDateTime roundUpTime(LocalDateTime time){
-            int remainder= time.getMinute() % 15;
-     if (remainder >=7) {
-        return time.plusMinutes(15-remainder);
-     }
-         return time.minusMinutes(remainder);
-
+    public LocalDateTime roundUpTime(LocalDateTime time) {
+        int remainder = time.getMinute() % 15;
+        if (remainder >= 7) {
+            return time.plusMinutes(15 - remainder);
+        }
+        return time.minusMinutes(remainder);
     }
-
 }
